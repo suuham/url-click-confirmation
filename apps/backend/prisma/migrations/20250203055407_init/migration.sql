c24f6581b09f640c4a9a58b53725c166bed565e0
karasuneo <yusei4520030401@gmail.com>
2025-02-03 14:59:24 +0900

    fix: DB内でのインデックスを変更

--- apps/backend/prisma/migrations/20250203055407_init/migration.sql
+++ apps/backend/prisma/migrations/20250203055407_init/migration.sql
@@ -2,10 +2,10 @@
 CREATE TABLE "company-view-urls" (
     "id" TEXT NOT NULL,
     "companyName" VARCHAR(256) NOT NULL,
-    "is_banned" BOOLEAN NOT NULL DEFAULT false,
+    "is_viewed" BOOLEAN NOT NULL DEFAULT false,
     "is_deleted" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_at" TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "company-view-urls_pkey" PRIMARY KEY ("id")
 );
