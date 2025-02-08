85f026b6cf557ce877723275b16bcf0efbbb81f9
karasuneo <yusei4520030401@gmail.com>
2025-02-08 17:12:32 +0900

    add: リクエストボディの解析を行う関数の定義

---
+++ apps/backend/src/utils/validateRequestBody.ts
@@ -0,0 +1,27 @@
+import type { Context } from "hono";
+import type { z } from "zod";
+import type { ErrorResponse } from "~/schema/error";
+
+export const validateRequestBody = async <T>(
+	c: Context,
+	schema: z.ZodSchema<T>,
+): Promise<
+	{ success: true; data: T } | { success: false; error: ErrorResponse }
+> => {
+	const body = await c.req.json();
+	const result = schema.safeParse(body);
+
+	if (!result.success) {
+		return {
+			success: false,
+			error: {
+				error: {
+					message: "Invalid request body",
+					details: result.error.errors.map((error) => error.message),
+				},
+			} satisfies ErrorResponse,
+		};
+	}
+
+	return { success: true, data: result.data };
+};
