2ec4d1e74840a67a0eca475fc20d664589f96eba
karasuneo <yusei4520030401@gmail.com>
2025-02-08 23:01:15 +0900

    refactor: 戻り値の型を明記

--- apps/backend/src/utils/getRequestOrigin.ts
+++ apps/backend/src/utils/getRequestOrigin.ts
@@ -3,7 +3,7 @@ import type { Context } from "hono";
 export const getAccessJudgmentUrl = (
 	c: Context,
 	accessJudgmentUrlId: string,
-) => {
+): string => {
 	const proto = c.req.header("x-forwarded-proto") || "http";
 	const host = c.req.header("host");
 	return `${proto}://${host}/access-judgment-url/${accessJudgmentUrlId}`;
