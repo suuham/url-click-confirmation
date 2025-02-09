64b7fd9856234c43720c95d8b81e75b993afdce5
karasuneo <yusei4520030401@gmail.com>
2025-02-09 16:08:59 +0900

    add: アクセス判定URLのアクセス情報を取得する関数の作成

--- apps/backend/src/handlers/getAccessJudgmentUrls.ts
+++ apps/backend/src/handlers/getAccessJudgmentUrls.ts
@@ -1,7 +1,16 @@
 import type { RouteHandler } from "@hono/zod-openapi";
+import type { Context } from "hono";
 import type { z } from "zod";
+import { getAccessJudgmentUrls } from "~/models/AccessJudgmentUrl";
+import { getAccessJudgmentUrlLogsByAccessJudgmentUrlId } from "~/models/AccessJudgmentUrlLog";
+import { getBaseUrlById } from "~/models/BaseUrl";
+import { getCompanyById } from "~/models/Company";
 import type { getAccessJudgmentUrlsRoute } from "~/routers/accessJudgmentUrl";
-import type { getAccessJudgmentUrlsResponseSchema } from "~/schema/accessJudgmentUrl";
+import {
+	getAccessJudgmentUrlsQuerySchema,
+	type getAccessJudgmentUrlsResponseSchema,
+} from "~/schema/accessJudgmentUrl";
+import { getAccessJudgmentUrl } from "~/utils/getRequestOrigin";
 
 type GetAccessJudgmentUrlsResponse = z.infer<
 	typeof getAccessJudgmentUrlsResponseSchema
@@ -9,28 +18,55 @@ type GetAccessJudgmentUrlsResponse = z.infer<
 
 export const getAccessJudgmentUrlsHandler: RouteHandler<
 	typeof getAccessJudgmentUrlsRoute
-> = (c) => {
+> = async (c: Context) => {
+	const { companyName, baseUrl, baseUrlTitle, limit, offset, sort, order } =
+		getAccessJudgmentUrlsQuerySchema.parse(c.req.query());
+
+	const accessJudgmentUrls = await getAccessJudgmentUrls(
+		limit,
+		offset,
+		sort,
+		order,
+	);
+
 	const response: GetAccessJudgmentUrlsResponse = {
-		accessJudgmentUrls: [
-			{
-				company: {
-					id: "1",
-					name: "Google",
-				},
-				baseUrl: {
-					id: "1",
-					url: "https://google.com",
-				},
-				accessJudgmentUrl: {
-					id: "1",
-					url: "https://google.com",
-					viewCount: 1,
-					lastViewedAt: 1560000000,
-					viewedAts: [1560000000, 1560000000],
-				},
-			},
-		],
+		accessJudgmentUrls: await Promise.all(
+			accessJudgmentUrls.map(async (accessJudgmentUrl) => {
+				const accessJudgmentUrlLogs =
+					await getAccessJudgmentUrlLogsByAccessJudgmentUrlId(
+						accessJudgmentUrl.id,
+					);
+				const company = (await getCompanyById(accessJudgmentUrl.companyId))!;
+				const baseUrl = (await getBaseUrlById(accessJudgmentUrl.baseUrlId))!;
+				const lastViewedAt = accessJudgmentUrlLogs
+					.map((log) => log.createdAt.getTime())
+					.reduce((a, b) => Math.max(a, b), 0);
+				const viewedAts = accessJudgmentUrlLogs
+					.map((log) => log.createdAt.getTime())
+					.sort((a, b) => b - a)
+					.slice(1);
+
+				return {
+					company: {
+						id: company.id,
+						name: company.name,
+					},
+					baseUrl: {
+						id: baseUrl.id,
+						title: baseUrl.title,
+						url: baseUrl.url,
+					},
+					accessJudgmentUrl: {
+						id: accessJudgmentUrl.id,
+						url: getAccessJudgmentUrl(c, accessJudgmentUrl.id),
+						viewCount: accessJudgmentUrlLogs.length,
+						lastViewedAt: lastViewedAt,
+						viewedAts: viewedAts,
+					},
+				};
+			}),
+		),
 	};
 
-	return c.json(response);
+	return c.json(response, 200);
 };
