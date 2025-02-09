75437f10e3690f1fad65ea98c4939c7ef7e963e7
karasuneo <yusei4520030401@gmail.com>
2025-02-09 16:01:18 +0900

    add: クエリパラメータ用のスキーマ定義

--- apps/backend/src/schema/accessJudgmentUrl.ts
+++ apps/backend/src/schema/accessJudgmentUrl.ts
@@ -1,4 +1,36 @@
 import { z } from "zod";
+import { unixTimestampSchema } from "./time";
+
+export const getAccessJudgmentUrlsQuerySchema = z.object({
+	companyName: z.string().optional().openapi({
+		description: "企業名",
+		example: "株式会社ホノ",
+	}),
+	baseUrl: z.string().optional().openapi({
+		description: "アクセス判定URL発行のベースURL",
+		example: "https://super-hamster.com/",
+	}),
+	baseUrlTitle: z.string().optional().openapi({
+		description: "アクセス判定URL発行のベースURLのタイトル",
+		example: "スーパーハムスター",
+	}),
+	limit: z.number().default(50).openapi({
+		description: "取得するアクセス判定URLの最大数",
+		example: 50,
+	}),
+	offset: z.number().default(0).openapi({
+		description: "取得するアクセス判定URLのオフセット",
+		example: 0,
+	}),
+	sort: z.string().default("createdAt").openapi({
+		description: "アクセス判定URLのソート順",
+		example: "createdAt",
+	}),
+	order: z.string().default("desc").openapi({
+		description: "アクセス判定URLのソート順",
+		example: "desc",
+	}),
+});
 
 export const getAccessJudgmentUrlsResponseSchema = z.object({
 	accessJudgmentUrls: z.array(
@@ -18,6 +50,10 @@ export const getAccessJudgmentUrlsResponseSchema = z.object({
 					description: "アクセス判定URL発行のベースURLのID",
 					example: "twe07rynom3fix89ub0aknnz",
 				}),
+				title: z.string().openapi({
+					description: "アクセス判定URL発行のベースURLのタイトル",
+					example: "スーパーハムスター紹介資料",
+				}),
 				url: z.string().openapi({
 					description: "アクセス判定URL発行のベースURL",
 					example: "https://super-hamster.com/",
@@ -36,14 +72,17 @@ export const getAccessJudgmentUrlsResponseSchema = z.object({
 					description: "アクセス判定URLの閲覧回数",
 					example: 3,
 				}),
-				lastViewedAt: z.number().openapi({
+				lastViewedAt: unixTimestampSchema.optional().openapi({
 					description: "アクセス判定URLの最終アクセス日時",
 					example: 1633824000000,
 				}),
-				viewedAts: z.array(z.number()).openapi({
-					description: "アクセス判定URLのアクセス日時のリスト",
-					example: [1633824000000, 1633824000000],
-				}),
+				viewedAts: z
+					.array(unixTimestampSchema)
+					.optional()
+					.openapi({
+						description: "アクセス判定URLのアクセス日時のリスト",
+						example: [1633824000000, 1633824000000],
+					}),
 			}),
 		}),
 	),
