6ea95db7e6f3d19472931554811836807d77a421
karasuneo <yusei4520030401@gmail.com>
2025-02-09 18:12:04 +0900

    add: 企業名をライク検索する関数を定義

--- apps/backend/src/models/Company.ts
+++ apps/backend/src/models/Company.ts
@@ -12,6 +12,14 @@ export const getCompanyById = async (id: string): Promise<Company | null> => {
 	return await prismaClient.company.findUnique({ where: { id } });
 };
 
+export const getCompaniesByNameLike = async (
+	name: string,
+): Promise<Company[]> => {
+	return await prismaClient.company.findMany({
+		where: { name: { contains: name } },
+	});
+};
+
 export const getCompaniesByNames = async (
 	names: string[],
 ): Promise<Company[]> => {
