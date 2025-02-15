12de3d2f1a353c8cc19506e32a58f6b0d221b453
karasuneo <yusei4520030401@gmail.com>
2025-02-15 16:40:07 +0900

    feat: UploadFileページのスタイルを追加し、ダウンロードボタンとファイルアップローダーを配置

--- apps/frontend/src/pages/UploadFile/index.tsx
+++ apps/frontend/src/pages/UploadFile/index.tsx
@@ -1,7 +1,22 @@
+import { DownloadSampleCsvButton } from "@/features/DownloadSampleCsv/components/DownloadSampleCsvButton";
+import { CreateButton } from "@/features/UploadFile/components/CreateButton";
+import { CsvFileUploader } from "@/features/UploadFile/components/CsvFileUploader";
+
+import styles from "./index.module.scss";
 export function UploadFilePage() {
 	return (
-		<div>
-			<h1>Upload File</h1>
+		<div className={styles["upload-page-block"]}>
+			<div className={styles["upload-element-block"]}>
+				<div className={styles["download-element"]}>
+					<DownloadSampleCsvButton />
+				</div>
+				<div className={styles["uploader-element"]}>
+					<CsvFileUploader />
+				</div>
+			</div>
+			<div>
+				<CreateButton />
+			</div>
 		</div>
 	);
 }
