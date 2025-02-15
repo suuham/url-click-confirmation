c6df1c6a8e99c75dfc1f50f2a7d3e77a735df8f7
karasuneo <yusei4520030401@gmail.com>
2025-02-15 16:38:51 +0900

    feat: Buttonコンポーネントを追加し、スタイルとサイズマップを定義

---
+++ apps/frontend/src/components/Button/const/index.ts
@@ -0,0 +1,17 @@
+export const BUTTON_SIZE_MAP: {
+	[key: string]: { width: string; height: string };
+} = {
+	xs: { width: "8rem", height: "2rem" },
+	sm: { width: "12rem", height: "2.5rem" },
+	md: { width: "16rem", height: "3rem" },
+	lg: { width: "24rem", height: "3.5rem" },
+	xl: { width: "32rem", height: "4rem" },
+};
+
+export const BUTTON_FONT_SIZE_MAP: { [key: string]: { fontSize: string } } = {
+	xs: { fontSize: "0.75rem" },
+	sm: { fontSize: "1rem" },
+	md: { fontSize: "1.25rem" },
+	lg: { fontSize: "1.5rem" },
+	xl: { fontSize: "2rem" },
+};
---
+++ apps/frontend/src/components/Button/index.tsx
@@ -0,0 +1,46 @@
+import colorStyles from "@/styles/colors.module.scss";
+
+import type { Size } from "@/types/size";
+import type { CSSProperties } from "react";
+import { BUTTON_FONT_SIZE_MAP, BUTTON_SIZE_MAP } from "./const";
+import styles from "./index.module.scss";
+
+type Props = {
+	text: string;
+	type?: "button" | "submit" | "reset";
+	size?: Size;
+	backgroundColor?: string;
+	isDisable?: boolean;
+	isOutline?: boolean;
+	onClick: () => void;
+};
+
+export function Button({
+	text,
+	type = "button",
+	size = "md",
+	backgroundColor = colorStyles.buttonColorBlue,
+	isDisable = false,
+	isOutline = false,
+	onClick,
+}: Props) {
+	const buttonStyle: CSSProperties = {
+		color: isOutline ? backgroundColor : colorStyles.buttonTextColor,
+		backgroundColor: isOutline ? "transparent" : backgroundColor,
+		border: isOutline ? `3px solid ${backgroundColor}` : "none",
+		...BUTTON_SIZE_MAP[size],
+		...BUTTON_FONT_SIZE_MAP[size],
+	};
+
+	return (
+		<button
+			disabled={isDisable}
+			className={styles["button"]}
+			style={buttonStyle}
+			type={type}
+			onClick={onClick}
+		>
+			{text}
+		</button>
+	);
+}
