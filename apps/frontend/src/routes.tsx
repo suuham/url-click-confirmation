97f2a7163dd2de8a7117641e6268128c35a4d787
karasuneo <yusei4520030401@gmail.com>
2025-02-20 20:52:39 +0900

    feat: ヘッダーのスタイルを修正し、ファイルアップロードページと完了ページのルートを追加

--- apps/frontend/src/routes.tsx
+++ apps/frontend/src/routes.tsx
@@ -2,11 +2,19 @@ import { Routes as ReactRouterRoutes, Route } from "react-router";
 import { ROUTES } from "./constants";
 import { AccessJudgmentListPage } from "./pages/AccessJudgmentList";
 import { UploadFilePage } from "./pages/UploadFile";
+import { DownloadAccessJudgmentUrlFilePage } from "./pages/DownloadAccessJudgmentUrlFile";
 
 export function Routes() {
 	return (
 		<ReactRouterRoutes>
-			<Route element={<UploadFilePage />} path={ROUTES.HOME} />
+			<Route
+				element={<UploadFilePage />}
+				path={ROUTES.ACCESS_JUDGMENT_LIST_FILE_UPLOAD_PAGE}
+			/>
+			<Route
+				element={<DownloadAccessJudgmentUrlFilePage />}
+				path={ROUTES.ACCESS_JUDGMENT_LIST_FILE_COMPLETE_PAGE}
+			/>
 			<Route
 				element={<AccessJudgmentListPage />}
 				path={ROUTES.ACCESS_JUDGMENT_LIST}
