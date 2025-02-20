6238f65f14354a9964a71dd1877e535eab7ade1b
karasuneo <yusei4520030401@gmail.com>
2025-02-20 18:27:00 +0900

    feat: CSVファイルのドラッグ＆ドロップアップロード機能を追加し、エラーハンドリングを強化

--- apps/frontend/src/features/UploadFile/hooks/useUploadFile.ts
+++ apps/frontend/src/features/UploadFile/hooks/useUploadFile.ts
@@ -1,28 +1,86 @@
 import { useRef, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { useAtom, useSetAtom } from "jotai";
+import { downloadFileAtom, uploadedFileAtom } from "@/stores";
+
+import { convertCsvToJson, convertJsonToCsv } from "../functions/convertToJson";
+import { readCsvFile } from "@/utils/readFile";
+import { isValidCsvFormat } from "../functions/isValidCsvFormat";
+import { postAccessJudgmentUrl } from "../api/postAccessJudgmentUrl";
 
 export function useUploadFile() {
 	const fileInputRef = useRef<HTMLInputElement>(null);
-	const [file, setFile] = useState<File | null>(null);
+	const [error, setError] = useState<string | null>(null);
+	const [isLoading, setIsLoading] = useState(false);
+	const [uploadFile, setUploadFile] = useAtom(uploadedFileAtom);
+	const setDownloadFile = useSetAtom(downloadFileAtom);
 
-	const handleButtonClick = () => {
+	const handleButtonClick = async () => {
 		fileInputRef.current?.click();
 	};
 
-	const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		if (event.target.files) {
-			setFile(event.target.files[0]);
+	const handleFileUpload = async () => {
+		console.log("こんにちは");
+		try {
+			setIsLoading(true);
+			console.log("こんにちは", uploadFile);
+
+			if (!uploadFile) {
+				setError("ファイルを選択してください");
+				return;
+			}
+
+			const _uploadCsv = await readCsvFile(uploadFile);
+			if (!isValidCsvFormat(_uploadCsv)) {
+				setError("CSVファイルの形式が正しくありません");
+				return;
+			}
+
+			console.log("こんにちは殺すぞ");
+			const request = convertCsvToJson(_uploadCsv);
+			const response = await postAccessJudgmentUrl(
+				request.createAccessJudgmentUrlsRequest,
+			);
+
+			console.log("こんにちは", response);
+			console.log("こんにちは", convertJsonToCsv(response));
+
+			setDownloadFile(
+				new Blob([convertJsonToCsv(response)], { type: "text/csv" }),
+			);
+		} catch (err) {
+			setError("エラーが発生しました");
+		} finally {
+			setIsLoading(false);
 		}
+
+		setIsLoading(false);
 	};
 
-	const handleFileUpload = () => {
-		console.log(file);
-	};
+	const { getRootProps, getInputProps, isDragActive } = useDropzone({
+		accept: { "text/csv": [".csv"] },
+		onDrop: async (acceptedFiles, fileRejections) => {
+			if (fileRejections.length > 0) {
+				setError("CSVファイルのみアップロードできます");
+				return;
+			}
+
+			if (acceptedFiles.length > 0) {
+				setUploadFile(acceptedFiles[0]);
+				setError(null);
+			}
+		},
+		onDragEnter: () => setError(null),
+	});
 
 	return {
 		fileInputRef,
-		file,
+		error,
+		isDragActive,
+		isLoading,
 		handleButtonClick,
-		handleFileChange,
 		handleFileUpload,
+		getRootProps,
+		getInputProps,
 	};
 }
