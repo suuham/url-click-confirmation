c20094d08af96fa9e18d605d5d92349a146723e5
karasuneo <yusei4520030401@gmail.com>
2025-02-20 18:26:50 +0900

    feat: アップロードファイルとダウンロードファイルの状態管理用アトムを追加

--- apps/frontend/src/stores/index.ts
+++ apps/frontend/src/stores/index.ts
@@ -2,3 +2,5 @@ import type { ROUTES } from "@/constants";
 import { atom } from "jotai";
 
 export const currentPageNameAtom = atom<ROUTES>("/");
+export const uploadedFileAtom = atom<File | null>(null);
+export const downloadFileAtom = atom<Blob | null>(null);
