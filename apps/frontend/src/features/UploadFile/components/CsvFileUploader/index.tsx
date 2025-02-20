5b95ec00ed32cc6d0d9d16adc86708e6cffb2b00
karasuneo <yusei4520030401@gmail.com>
2025-02-20 18:28:30 +0900

    feat: CSVファイルアップローダーにエラーメッセージとアップロードファイルのサマリーを追加

--- apps/frontend/src/features/UploadFile/components/CsvFileUploader/index.tsx
+++ apps/frontend/src/features/UploadFile/components/CsvFileUploader/index.tsx
@@ -1,29 +1,58 @@
 import { Button } from "@/components/Button";
+import { UploadedFileSummary } from "../UploadedFileSummary";
 import styles from "./index.module.scss";
-import { useUploadFile } from "../../hooks/useUploadFile";
+import { useAtomValue } from "jotai";
+import { uploadedFileAtom } from "@/stores";
 
-export function CsvFileUploader() {
-	const { fileInputRef, handleButtonClick, handleFileChange } = useUploadFile();
+type Props = {
+	fileInputRef: React.RefObject<HTMLInputElement | null>;
+	error: string | null;
+	isDragActive: boolean;
+	handleButtonClick: () => void;
+	getRootProps: () => Record<string, unknown>;
+	getInputProps: () => Record<string, unknown>;
+};
+
+export function CsvFileUploader({
+	fileInputRef,
+	error,
+	isDragActive,
+	handleButtonClick,
+	getRootProps,
+	getInputProps,
+}: Props) {
+	const uploadFile = useAtomValue(uploadedFileAtom);
 
 	return (
-		<div className={styles["uploader-block"]}>
+		<div className={styles["uploader-block"]} {...getRootProps()}>
+			<input
+				{...getInputProps()}
+				ref={fileInputRef}
+				style={{ display: "none" }}
+			/>
 			<div>
-				<h2>CSVをドラック&ドロップ</h2>
+				<h2>CSVをドラッグ&ドロップ</h2>
 			</div>
-			<div>
-				<input
-					type="file"
-					ref={fileInputRef}
-					onChange={handleFileChange}
-					style={{ display: "none" }}
-				/>
-				<Button
-					text="ファイルを選択"
-					size="lg"
-					isOutline
-					onClick={handleButtonClick}
-				/>
+			<div className={styles["button-block"]}>
+				{isDragActive ? (
+					<p className={styles["drop-text"]}>ここにドロップしてください</p>
+				) : (
+					<>
+						<Button
+							text="ファイルを選択"
+							size="lg"
+							isOutline
+							onClick={handleButtonClick}
+						/>
+					</>
+				)}
+				{error && <p className={styles["error-text"]}>{error}</p>}
 			</div>
+			{uploadFile && (
+				<div>
+					<UploadedFileSummary name={uploadFile.name} size={uploadFile.size} />
+				</div>
+			)}
 		</div>
 	);
 }
