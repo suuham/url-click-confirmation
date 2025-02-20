be782aebf13fa03c41a12fa87ed8ffebdb40c145
karasuneo <yusei4520030401@gmail.com>
2025-02-20 20:50:58 +0900

    feat: downloadFileAtomの型をBlobからFileに変更

--- apps/frontend/src/stores/index.ts
+++ apps/frontend/src/stores/index.ts
@@ -3,4 +3,4 @@ import { atom } from "jotai";
 
 export const currentPageNameAtom = atom<ROUTES>("/");
 export const uploadedFileAtom = atom<File | null>(null);
-export const downloadFileAtom = atom<Blob | null>(null);
+export const downloadFileAtom = atom<File | null>(null);
