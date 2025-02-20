03b064c638f31219dadc14fe4303ea459f9994f7
karasuneo <yusei4520030401@gmail.com>
2025-02-20 20:52:23 +0900

    feat: アクセス判断リストのファイルアップロードページと完了ページのルートを追加

--- apps/frontend/src/constants/index.ts
+++ apps/frontend/src/constants/index.ts
@@ -1,6 +1,10 @@
 export const ROUTES = {
 	// biome-ignore lint/style/useNamingConvention:
-	HOME: "/",
+	ACCESS_JUDGMENT_LIST_FILE_UPLOAD_PAGE: "/",
+
+	// biome-ignore lint/style/useNamingConvention:
+	ACCESS_JUDGMENT_LIST_FILE_COMPLETE_PAGE: "/complete",
+
 	// biome-ignore lint/style/useNamingConvention:
 	ACCESS_JUDGMENT_LIST: "/access-judgment-urls",
 } as const;
