87d8d7a4e279edb4e36c781635f0711d30d6a972
karasuneo <yusei4520030401@gmail.com>
2025-02-20 18:28:02 +0900

    feat: CSVとJSON間の変換機能を追加

---
+++ apps/frontend/src/features/UploadFile/functions/accessJudgmentCsv.ts
@@ -0,0 +1,34 @@
+import {
+	CreateAccessJudgmentUrlsRequest,
+	CreateAccessJudgmentUrlsResponse,
+} from "@/types/scheme";
+
+export function convertCsvToJson(csv: string): {
+	createAccessJudgmentUrlsRequest: CreateAccessJudgmentUrlsRequest;
+} {
+	const lines = csv.trim().split("\n");
+
+	const data = lines.slice(1).map((line) => {
+		const [companyName, baseUrl] = line.split(",");
+
+		return {
+			companyName,
+			baseUrl: {
+				title: "",
+				url: baseUrl,
+			},
+		};
+	});
+
+	return { createAccessJudgmentUrlsRequest: { accessJudgmentUrlInfo: data } };
+}
+
+export function convertJsonToCsv(
+	json: CreateAccessJudgmentUrlsResponse,
+): string {
+	const lines = json.accessJudgmentUrls.map((accessJudgmentUrl) => {
+		return `${accessJudgmentUrl.companyName},${accessJudgmentUrl.baseUrl.url}`;
+	});
+
+	return ["company_name,base_url", ...lines].join("\n");
+}
