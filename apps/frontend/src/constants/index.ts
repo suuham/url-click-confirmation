c1ec5f633a050742743230963d80b21ecb9d4ccc
karasuneo <yusei4520030401@gmail.com>
2025-02-23 14:05:18 +0900

    feat: ルート定数の型名を修正し、命名規則に従う

--- apps/frontend/src/constants/index.ts
+++ apps/frontend/src/constants/index.ts
@@ -1,16 +1,11 @@
 export const ROUTES = {
-	// biome-ignore lint/style/useNamingConvention:
 	ACCESS_JUDGMENT_LIST_FILE_UPLOAD_PAGE: "/",
 
-	// biome-ignore lint/style/useNamingConvention:
 	ACCESS_JUDGMENT_LIST_FILE_COMPLETE_PAGE: "/complete",
 
-	// biome-ignore lint/style/useNamingConvention:
 	ACCESS_JUDGMENT_LIST: "/access-judgment-urls",
 } as const;
 
-// biome-ignore lint/style/useNamingConvention:
-export type ROUTES = (typeof ROUTES)[keyof typeof ROUTES];
+export type Routes = (typeof ROUTES)[keyof typeof ROUTES];
 
-// biome-ignore lint/style/useNamingConvention:
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
