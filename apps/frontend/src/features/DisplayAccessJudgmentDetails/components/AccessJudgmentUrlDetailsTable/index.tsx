78770c80f3d06b2552e002de4bfc12dd2899872d
karasuneo <yusei4520030401@gmail.com>
2025-02-23 14:34:00 +0900

    feat: アクセス判断URL詳細表示用のテーブルコンポーネントを追加

---
+++ apps/frontend/src/features/DisplayAccessJudgmentDetails/components/AccessJudgmentUrlDetailsTable/index.tsx
@@ -0,0 +1,84 @@
+import type { GetAccessJudgmentUrlsResponse } from "@/types/scheme";
+
+import { AccessCount } from "../AccessCount";
+import { AccessStatusIcon } from "../AccessStatusIcon";
+import { AccessUrl } from "../AccessUrl";
+import styles from "./index.module.scss";
+
+type Props = {
+	accessJudgmentUrlsDetails: GetAccessJudgmentUrlsResponse | null;
+};
+
+export function AccessJudgmentUrlDetailsTable({
+	accessJudgmentUrlsDetails,
+}: Props) {
+	return (
+		<div className={styles["access-judgment-url-details-list"]}>
+			<table>
+				<thead>
+					<tr>
+						<th scope="col">企業名</th>
+						<th scope="col">ベースURL</th>
+						<th scope="col">アクセス判定URL</th>
+						<th scope="col">状態</th>
+						<th scope="col">アクセス数</th>
+						<th scope="col">アクセス日時</th>
+						<th scope="col">URL発行日</th>
+					</tr>
+				</thead>
+				<tbody>
+					{accessJudgmentUrlsDetails
+						? accessJudgmentUrlsDetails.accessJudgmentUrls.map(
+								(accessJudgmentUrl) => (
+									<tr key={accessJudgmentUrl.accessJudgmentUrl.id}>
+										<td>{accessJudgmentUrl.company.name}</td>
+										<td>
+											<AccessUrl url={accessJudgmentUrl.baseUrl.url} />
+										</td>
+										<td>
+											<AccessUrl
+												url={accessJudgmentUrl.accessJudgmentUrl.url}
+											/>
+										</td>
+										<td>
+											<AccessStatusIcon
+												accessStatus={
+													accessJudgmentUrl.accessJudgmentUrl.viewCount > 0
+												}
+											/>
+										</td>
+										<td>
+											<AccessCount
+												accessCount={
+													accessJudgmentUrl.accessJudgmentUrl.viewCount
+												}
+											/>
+										</td>
+										<td>
+											{accessJudgmentUrl.accessJudgmentUrl.lastViewedAt === 0
+												? "-"
+												: new Date(
+														accessJudgmentUrl.accessJudgmentUrl.createdAt,
+													).toLocaleString("ja-JP", {
+														year: "numeric",
+														month: "2-digit",
+														day: "2-digit",
+														hour: "2-digit",
+														minute: "2-digit",
+														hour12: false,
+													})}
+										</td>
+										<td>
+											{new Date(
+												accessJudgmentUrl.accessJudgmentUrl.createdAt,
+											).toLocaleDateString()}
+										</td>
+									</tr>
+								),
+							)
+						: null}
+				</tbody>
+			</table>
+		</div>
+	);
+}
