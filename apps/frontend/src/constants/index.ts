61a3bcbf3dba760daa7270c12ce2e6fb95d983e2
karasuneo <yusei4520030401@gmail.com>
2025-02-26 10:27:21 +0900

    refactor: ルート定数の命名規則に関するlintを無視するコメントを追加

--- apps/frontend/src/constants/index.ts
+++ apps/frontend/src/constants/index.ts
@@ -1,8 +1,9 @@
 export const ROUTES = {
+	// biome-ignore lint/style/useNamingConvention:
 	ACCESS_JUDGMENT_LIST_FILE_UPLOAD_PAGE: "/",
-
+	// biome-ignore lint/style/useNamingConvention:
 	ACCESS_JUDGMENT_LIST_FILE_COMPLETE_PAGE: "/complete",
-
+	// biome-ignore lint/style/useNamingConvention:
 	ACCESS_JUDGMENT_LIST: "/access-judgment-urls",
 } as const;
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
