b7723e41add9a7ea257b4668787f16eac81f6af3
okamonster <okamonster0615@gmail.com>
2025-06-13 04:37:04 +0900

    feat:add pagenation

--- apps/frontend/src/features/DisplayAccessJudgmentDetails/components/index.tsx
+++ apps/frontend/src/features/DisplayAccessJudgmentDetails/components/index.tsx
@@ -1,14 +1,20 @@
 import { Button } from "@/components/Button";
+import { useDownloadCsv } from "../hooks/useDownloadCsv";
 import { useSearchQuery } from "../hooks/useSearchQuery";
 import { AccessJudgmentUrlDetailsTable } from "./AccessJudgmentUrlDetailsTable";
 import { AccessJudgmentUrlSearchBar } from "./AccessJudgmentUrlSearchBar";
-
-import { useDownloadCsv } from "../hooks/useDownloadCsv";
+import { PaginationAction } from "./PaginationAction";
 import styles from "./index.module.scss";
 
 export function AccessJudgmentUrlSearchTable() {
-	const { accessJudgmentUrlsDetails, onChangeSearchQuery, handleSearch } =
-		useSearchQuery();
+	const {
+		accessJudgmentUrlsDetails,
+		onChangeSearchQuery,
+		handleSearch,
+		handlePageChange,
+		currentPage,
+		totalPages,
+	} = useSearchQuery();
 
 	const handleDownloadCsv = useDownloadCsv();
 
@@ -30,6 +36,13 @@ export function AccessJudgmentUrlSearchTable() {
 			<div className={styles["access-judgment-url-details-list"]}>
 				<AccessJudgmentUrlDetailsTable
 					accessJudgmentUrlsDetails={accessJudgmentUrlsDetails}
+				/>
+			</div>
+			<div className={styles["pagination-container"]}>
+				<PaginationAction
+					currentPage={currentPage}
+					totalPages={totalPages}
+					onPageChange={handlePageChange}
 				/>
 			</div>
 		</div>
