8d0539415bc08604bdda772d74488e75634e2beb
andmohiko <andmohiko@gmail.com>
2025-07-04 18:44:03 +0900

    fix: serve app at node runtime

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,11 @@
+import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 
 import { swaggerUI } from "@hono/swagger-ui";
 import { router } from "./routers";
 
+const port = Number(process.env.PORT) || 8080;
 const app = new Hono();
 
 app.use("*", cors());
@@ -11,5 +13,13 @@ app.use("*", cors());
 app.route("/", router);
 app.get("/ui", swaggerUI({ url: "/doc" }));
 
-// biome-ignore lint/style/noDefaultExport:
-export default app;
+app.onError((e, c) => {
+	// biome-ignore lint/suspicious/noConsole: <explanation>
+	console.error("Error:", e);
+	return c.text("Internal Server Error", 500);
+});
+
+serve({
+	fetch: app.fetch,
+	port,
+});
